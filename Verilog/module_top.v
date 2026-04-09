84741a29979bb4cd738854adbe022b394f7cbb60
Roshan <rks54@duke.edu>
2026-04-09 16:00:29 -0400

    test_mem_evict passing

--- Verilog/module_top.v
+++ Verilog/module_top.v
@@ -276,7 +276,7 @@ module system_top (
     );
 
     L1I_cache #(
-        .INIT_FILE("Hex_files/text_mem_evict_c1.hex") 
+        .INIT_FILE("Hex_files/test_mem_evict_c1.hex") 
     ) core1_l1i (
         .clk(clk),
         .reset(reset),
--- Verilog/tb_system.v
+++ Verilog/tb_system.v
@@ -32,7 +32,7 @@ module tb_system();
         $display("System Booting... Cores are executing firmware.");
 
         // Let it run
-        #5000;
+        #500000;
 
         
         $display("Simulation Complete.");
