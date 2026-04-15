f7a6137c65764ac9d8b371485d5c0676b535f0e0
Roshan <rks54@duke.edu>
2026-04-15 09:00:58 -0400

    Project Done...for now

--- Verilog/C_programs/matmul_c1.c
+++ Verilog/C_programs/matmul_c1.c
@@ -6,7 +6,7 @@ void main() {
     volatile int* C = (int*)0x2080;
     volatile int* init_flag = (int*)0x20C0;
 
-    // Wait until Core 0 finishes initializing
+
     while(*init_flag == 0);
 
     // Compute bottom half of Matrix C (Rows 2 and 3)
@@ -15,7 +15,7 @@ void main() {
         for(int j = 0; j < SIZE; j++) {
             int sum = 0;
             for(int k = 0; k < SIZE; k++) {
-                // Reading A and B here forces C0 to downgrade its cache blocks from M to S
+
                 sum += A[i * SIZE + k] * B[k * SIZE + j];
             }
             C[i * SIZE + j] = sum;
@@ -23,7 +23,7 @@ void main() {
         }
     }
 
-    // Expected checksum for bottom half: 200
+    // Expected result for bottom half: 200
     __asm__ volatile ("mv x31, %0" : : "r" (checksum));
     __asm__ volatile ("li x30, 1");
     
